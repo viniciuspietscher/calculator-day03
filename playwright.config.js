054037a47b8f51ea5e5c49fd8ebc83fff83b02a0
Vinicius Pietscher <8095975+viniciuspietscher@users.noreply.github.com>
2023-01-05 17:14:18 -0500

    catch all routes

--- playwright.config.js
+++ playwright.config.js
@@ -1,11 +1,11 @@
-import { PlaywrightTestConfig, devices } from "@playwright/test";
-import path from "path";
+import { PlaywrightTestConfig, devices } from "@playwright/test"
+import path from "path"
 
 // Use process.env.PORT by default and fallback to port 3000
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 // Set webServer.url and use.baseURL with the location of the WebServer respecting the correct set port
-const baseURL = `http://localhost:${PORT}`;
+const baseURL = `http://localhost:${PORT}`
 
 // Reference: https://playwright.dev/docs/test-configuration
 const config = {
@@ -57,24 +57,23 @@ const config = {
         ...devices["Desktop Firefox"],
       },
     },
-    {
-      name: "Desktop Safari",
-      use: {
-        ...devices["Desktop Safari"],
-      },
-    },
-    // Test against mobile viewports.
-    {
-      name: "Mobile Chrome",
-      use: {
-        ...devices["Pixel 5"],
-      },
-    },
-    {
-      name: "Mobile Safari",
-      use: devices["iPhone 12"],
-    },
+    // {
+    //   name: "Desktop Safari",
+    //   use: {
+    //     ...devices["Desktop Safari"],
+    //   },
+    // },
+    // // Test against mobile viewports.
+    // {
+    //   name: "Mobile Chrome",
+    //   use: {
+    //     ...devices["Pixel 5"],
+    //   },
+    // },
+    // {
+    //   name: "Mobile Safari",
+    //   use: devices["iPhone 12"],
+    // },
   ],
-};
-export default config;
-
+}
+export default config
