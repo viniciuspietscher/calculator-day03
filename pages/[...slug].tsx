967382da8398a7185842fcc1f804ed03e588c3da
Vinicius Pietscher <8095975+viniciuspietscher@users.noreply.github.com>
2023-01-05 21:43:34 -0500

    redirect if no number

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -4,11 +4,16 @@ import { useEffect } from "react"
 export default function Comment() {
   const router = useRouter()
   const slug = (router.query.slug as string[]) || []
-
   const operations = ["add", "subtract", "multiply", "divide"]
 
   useEffect(() => {
     if (slug[0] && !operations.includes(slug[0])) {
+      router.push("/404")
+    }
+    if (slug[1] && isNaN(Number(slug[1]))) {
+      router.push("/404")
+    }
+    if (slug[2] && isNaN(Number(slug[2]))) {
       router.push("/404")
     }
   }, [router.query.slug])
