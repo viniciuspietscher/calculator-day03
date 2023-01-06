50858994165ab078d1bd6721436b5ec99d0bc558
Vinicius Pietscher <8095975+viniciuspietscher@users.noreply.github.com>
2023-01-05 21:38:04 -0500

    add 404, redirect if no operation

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -1,8 +1,17 @@
 import { useRouter } from "next/router"
+import { useEffect } from "react"
 
-const Comment = () => {
+export default function Comment() {
   const router = useRouter()
   const slug = (router.query.slug as string[]) || []
+
+  const operations = ["add", "subtract", "multiply", "divide"]
+
+  useEffect(() => {
+    if (slug[0] && !operations.includes(slug[0])) {
+      router.push("/404")
+    }
+  }, [router.query.slug])
 
   return (
     <>
@@ -10,5 +19,3 @@ const Comment = () => {
     </>
   )
 }
-
-export default Comment
